292532601eccd81f85ffe1c896ba308c12cc3e29
Magne Strømsnes <alaka@Alaka-PC>
2023-02-13 17:20:27 +0100

    Use pandas and parquet for archiving.
    Pandas proved a bad fit for storing incoming data. Numpy easily allows
    appending new data on increasing indexes. Dataframes didn't like adding
    new data neatly to existing columns. Pandas is more suitable for static
    datasets.

--- raspberry_listener/remotereader.py
+++ raspberry_listener/remotereader.py
@@ -2,24 +2,23 @@ from fabric import connection, transfer
 import pathlib
 import logging
 
+DATASET_FILE = "raspberry_log.parquet"
+
 
 class LogDownloader:
     def __init__(self):
         self.remote_dir = pathlib.Path("/home/.temp/")
-        self.cpu = "cpu_temp.npy"
-        self.time = "timestamp.npy"
-        self.local_timestamp_file, self.local_cpu_file = self._download_archived_files()
+        self.local_dataset_file = self._download_archived_file()
 
-    def _download_archived_files(self) -> tuple[str, str]:
+    def _download_archived_file(self) -> str:
         with connection.Connection(
             "friendlynas", user="alaka", connect_kwargs={"password": "greendino"}
         ) as nas:
             t = transfer.Transfer(nas)
-            cpu_file_result = t.get(self.remote_dir / self.cpu)
-            datetime_file_result = t.get(self.remote_dir / self.time)
-            result = datetime_file_result.local, cpu_file_result.local
+            dataset_file_result = t.get(self.remote_dir / DATASET_FILE)
+            result = dataset_file_result.local
             logging.info(f"Downloaded files to {result}")
             return result
 
     def get_latest_archive(self):
-        return self._download_archived_files()
+        return self._download_archived_file()
