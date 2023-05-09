03644f4345895346393ca69003a463e39f2a0082
Magne Strømsnes <alaka@Alaka-PC>
2023-05-09 13:44:09 +0200

    Remove old datastore and use pandas
    Remove the old datastore and download logic in favor of the validated
    dataframe. For the moment we still send numpy arrays to matplotlib. This
    stops us from using the unit for anything useful, but kept the changes
    smaller.
    
    Time Of Day widget creates a dataframe from the base arrays to calculate
    the buckets, so we might want to send the relevant data to the plots in
    the future to simplify things like that.

--- raspberry_listener/datamediator.py
+++ raspberry_listener/datamediator.py
@@ -1,130 +1,42 @@
-import numpy as np
 import pandas as pd
-from datatypes import DataType, DataHandler, DataSet
-from remotereader import LogDownloader
-from datetime import datetime
+from datatypes import DataSet
+from pandera.typing import DataFrame
+import pandera as pa
 
+from datatypes import SensorData
+from fastapi import HTTPException
+from datatypes import Sensor, SensorType
 
-def _changed_points_only(func):
-    last_point: dict[DataType, DataStore.DataPoint] = {}
-
-    def wrapper(self, datatype: DataType, datapoint: "DataStore.DataPoint"):
-        nonlocal last_point
-        if (
-            last_point.get(datatype) is not None
-            and last_point[datatype][1] != datapoint[1]
-        ):
-            func(self, datatype, last_point[datatype])
-            func(self, datatype, datapoint)
-        if last_point.get(datatype) is None:
-            func(self, datatype, datapoint)
-        last_point[datatype] = datapoint
-
-    return wrapper
-
-
-class DataStore:
-    DataPoint = tuple[datetime, int | float]
-
-    def __init__(self):
-        time_array = np.zeros(1024, dtype=datetime)
-        self._data: dict[DataHandler, tuple[np.ndarray, np.ndarray, int]] = dict()
-        for type in DataType.to_set():
-            self._data[type] = (
-                np.copy(time_array),
-                np.zeros(1024, dtype=type.datatype()),
-                0,
-            )
-
-    @_changed_points_only
-    def append(self, data_type: DataHandler, data_point: DataPoint):
-        time_array, data_array, cnt = self._data[data_type]
-        while cnt + 1 >= data_array.size:
-            self._resize_array(data_type)
-            _, data_array, cnt = self._data[data_type]
-        self._add_data_point(data_type, data_point)
-
-    def extend(self, data_type: DataHandler, data_range: DataSet):
-        time_array, data_array, cnt = self._data[data_type]
-        new_time, new_data = data_range
-        new_data_array = np.array(new_data)
-        new_time_array = np.array(new_time)
-        while new_data_array.size + cnt >= data_array.size:
-            self._resize_array(data_type)
-            _, data_array, cnt = self._data[data_type]
-        self._add_data_range(data_type, (new_time_array, new_data_array))
-
-    def overwrite_data(self, data_type, timestamp_array, data_array):
-        timestamp_array, data_array = self._drop_nan(timestamp_array, data_array)
-        self._data[data_type] = (timestamp_array, data_array, data_array.size)
-
-    def _drop_nan(self, timestamp_array, data_array):
-        finite_data = np.isfinite(data_array)
-        timestamp_array = timestamp_array[finite_data]
-        data_array = data_array[finite_data]
-        return timestamp_array, data_array
-
-    def _add_data_range(self, data_type: DataHandler, new_arrays: DataSet):
-        time_array, data_array, cnt = self._data[data_type]
-        new_time_array, new_data_array = new_arrays
-        bool_array = np.ones(data_array.size, dtype=np.bool_)
-        np.copyto(bool_array, np.zeros(cnt))
-        np.copyto(time_array, new_time_array, where=bool_array)
-        np.copyto(data_array, new_data_array, where=bool_array)
-        cnt = cnt + new_data_array.size
-        self._data[data_type] = (time_array, data_array, cnt)
-
-    def _add_data_point(self, data_type: DataHandler, data_point: DataPoint):
-        time_array, data_array, cnt = self._data[data_type]
-        time, data = data_point
-        time_array[cnt] = time
-        data_array[cnt] = data
-        cnt = cnt + 1
-        self._data[data_type] = (time_array, data_array, cnt)
-
-    def _resize_array(self, data_type: DataHandler):
-        current_size = self._data[data_type][0].size
-        new_size = 2 * current_size
-        time_array, data_array, cnt = self._data[data_type]
-        new_time_array = np.resize(time_array, new_size)
-        new_data_array = np.resize(data_array, new_size)
-        self._data[data_type] = (new_time_array, new_data_array, cnt)
-
-    def get_data(self, data_type: DataHandler) -> DataSet:
-        time, data, cnt = self._data[data_type]
-        return DataSet(time[:cnt], data[:cnt])
+from remotereader import get_latest_archive
 
 
 class DataMediator:
     def __init__(self):
-        self.archive = LogDownloader()
-        self._datastore = DataStore()
+        self._dataframe: DataFrame[SensorData] = self._load_dataframe()
 
-    def get_data(self, data_type: DataHandler) -> DataSet:
-        return self._datastore.get_data(data_type)
+    @pa.check_types
+    def _load_dataframe(self) -> DataFrame[SensorData]:
+        try:
+            dataframe = self._read_parquet_latest_archive()
+        except HTTPException:
+            dataframe = SensorData.example(size=0)
+        return dataframe
 
-    def gather_data(self, request: DataHandler):
-        dataset = self._read_parquet_latest_archive()
-        unpacked_dataset = self._unpack_dataframe(dataset)
-        for df_name in request.dataframe_names:
-            self._datastore.overwrite_data(
-                request, unpacked_dataset["index"], unpacked_dataset[df_name]
-            )
+    def get_data(self, sensor: Sensor, sensor_type: SensorType) -> DataSet:
+        self._dataframe = self._dataframe.sort_index()
+        series: pd.Series = self._dataframe.loc[sensor_type.value, sensor.value][
+            "reading"
+        ]
+        timearray = series.index.to_numpy()
+        dataarray = series.to_numpy()
+        return DataSet(timearray, dataarray)
 
-    def _read_parquet_latest_archive(self):
+    def gather_data(self):
+        self._dataframe = self._load_dataframe()
+
+    @pa.check_types
+    def _read_parquet_latest_archive(self) -> DataFrame[SensorData]:
         """Pandas and parquet are used to archive the data. Pandas serve as the interface to parquet, a binary file format supporting compression suitable for storage.
         Pandas dataframes are not suitable for storing live incoming data, as adding new data is cumbersome. The unpacked numpy arrays are much more suitable for this.
         """
-        return pd.read_parquet(self.archive.get_latest_archive())
-
-    @staticmethod
-    def _unpack_dataframe(dataframe: pd.DataFrame):
-        df_as_dict = {}
-        df_as_dict["index"] = dataframe.index.to_numpy(dtype=np.datetime64)
-        for datatype in DataType.to_set():
-            for column in datatype.dataframe_names:
-                if column in dataframe.columns:
-                    dtype = datatype.datatype()
-                    df_as_dict[column] = dataframe[column].to_numpy(dtype=dtype)
-
-        return df_as_dict
+        return pd.read_parquet(get_latest_archive())
--- raspberry_listener/remotereader.py
+++ raspberry_listener/remotereader.py
@@ -1,24 +1,17 @@
-from fabric import connection, transfer
-import pathlib
-import logging
-
-DATASET_FILE = "raspberry_log.parquet"
+import requests
+from fastapi import HTTPException
+from io import BytesIO
 
 
-class LogDownloader:
-    def __init__(self):
-        self.remote_dir = pathlib.Path("/home/.temp/")
-        self.local_dataset_file = self._download_archived_file()
+def download_archived_file() -> BytesIO:
+    response = requests.get("http://192.168.4.141:8000/archive")
+    if response.status_code != 200:
+        raise HTTPException(response.status_code, response.json())
+    buffer = BytesIO()
+    buffer.write(response.content)
+    print(response.headers["content-length"])
+    return buffer
 
-    def _download_archived_file(self) -> str:
-        with connection.Connection(
-            "friendlynas", user="alaka", connect_kwargs={"password": "greendino"}
-        ) as nas:
-            t = transfer.Transfer(nas)
-            dataset_file_result = t.get(self.remote_dir / DATASET_FILE)
-            result = dataset_file_result.local
-            logging.info(f"Downloaded files to {result}")
-            return result
 
-    def get_latest_archive(self):
-        return self._download_archived_file()
+def get_latest_archive():
+    return download_archived_file()
