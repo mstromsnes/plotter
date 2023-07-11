190c8e01477540d0c937cb1f70934ab6679393ab
Magne Strømsnes <alaka@Alaka-PC>
2023-07-11 16:35:29 +0200

    Update main to use new code from merged branches
    The structure has changed dramatically in the three merged branches.

--- raspberry_listener/main.py
+++ raspberry_listener/main.py
@@ -1,30 +1,35 @@
-from PySide6 import QtGui, QtCore, QtWidgets
-import mainwindow
-from datathread import DataThread
-from datamediator import DataMediator
 import logging
+
+from ui.dataplotterwindow import DataPlotterWindow
+from datamodels.sources import FrameHandler, SensorDataFrameHandler
+from datathread import DataThreadController
+from PySide6 import QtCore, QtWidgets
 
 
 def main():
     app = QtWidgets.QApplication()
-    data_source = DataMediator()
-    window = mainwindow.MainWindow(data_source)
-    datathread = DataThread(data_source, window)
+    available_datasets = ["Pi-sensors"]
 
+    def load_dataset(name: str) -> tuple[FrameHandler, DataThreadController]:
+        match name:
+            case "Pi-sensors":
+                handler = SensorDataFrameHandler()
+                thread = DataThreadController(handler)
+                data_collection_timer.timeout.connect(thread.update_data)
+                thread.finished.connect(window.update_visible_plot)
+                return handler, thread
+            case _:
+                raise KeyError(f"Dataset {name} not available")
+
+    window = DataPlotterWindow(available_datasets, load_dataset)
     data_collection_timer = QtCore.QTimer()
 
-    data_collection_timer.timeout.connect(datathread.gather_data)
-    datathread.finished.connect(window.update_visible_plot)
-    window.resize(800, 600)
+    window.resize(1200, 600)
     window.show()
     window.update_plots()
     data_collection_timer.timeout.emit()
     data_collection_timer.start(5000)
     app.exec()
-
-
-def gather_data(data_source: DataMediator):
-    data_source.merge_new_data_into_dataframe()
 
 
 if __name__ == "__main__":
