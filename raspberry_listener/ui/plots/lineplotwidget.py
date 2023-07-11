e4e2a399a221c663328de7e7f1345983fe58b273
Magne Strømsnes <alaka@Alaka-PC>
2023-07-11 16:22:15 +0200

    Add lineplotwidget to the package
    Also move the supporting widgets in lineplotwidget to their own modules.
    
    When everything was in the root folder, this would have been quite
    messy, but limited to an appropriate package it's quite natural to do
    and much neater.

---
+++ raspberry_listener/ui/plots/lineplotwidget.py
@@ -0,0 +1,31 @@
+import plotmanager
+from datamodels.sources.framehandler import FrameHandler
+from plotstrategies.line import LinePlot
+from PySide6 import QtCore, QtWidgets
+from ui.drawwidget import DrawWidget
+from .simplifyplotbutton import SimplifyPlotSpinBox
+
+
+class LinePlotWidget(DrawWidget):
+    def __init__(
+        self,
+        datasource: FrameHandler,
+        parent: QtWidgets.QWidget | None = None,
+        **kwargs,
+    ):
+        super().__init__(parent=parent)
+
+        self.datasource = datasource
+        self.navigation_layout.addWidget(SimplifyPlotSpinBox())
+        self.manager = plotmanager.OneAxesPrSensorTypeManager(self, "Timeseries")
+
+    @QtCore.Slot(tuple, bool)
+    def toggle_line(self, dataset_key: tuple, label: str | None, checked: bool):
+        if not checked:
+            self.manager.remove_plotting_strategy(*dataset_key)
+        else:
+            strategy = LinePlot(self.datasource, dataset_key, label)
+            self.manager.add_plotting_strategy(strategy, *dataset_key)
+
+    def plot(self):
+        self.manager.plot()
---
+++ raspberry_listener/ui/plots/medfilterbutton.py
@@ -0,0 +1,38 @@
+from PySide6 import QtWidgets, QtCore, QtGui
+from scipy.signal import medfilt
+
+
+class MedFilterButton(QtWidgets.QWidget):
+    valueChanged = QtCore.Signal(int)
+
+    class OddSpinBox(QtWidgets.QSpinBox):
+        def validate(self, text: str, pos: int):
+            validator = QtGui.QIntValidator()
+            validator.setBottom(self.minimum())
+            validator.setTop(self.maximum())
+            state = validator.validate(text, pos)
+            if state is QtGui.QValidator.State.Invalid:
+                return state
+            number = int(text)
+            if number % 2 == 1:
+                return QtGui.QValidator.State.Acceptable
+            return QtGui.QValidator.State.Invalid
+
+    def __init__(
+        self,
+        parent: QtWidgets.QWidget | None = None,
+    ):
+        super().__init__(parent)
+        self.spinbox = self.OddSpinBox()
+        self.label = QtWidgets.QLabel("Median Filter")
+        self.spinbox.setValue(1)
+        self.spinbox.setMinimum(1)
+        self.spinbox.setSingleStep(2)
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.addWidget(self.spinbox)
+        layout.addWidget(self.label)
+        self.spinbox.valueChanged.connect(self.valueChanged)
+
+    def medfilt(self, data):
+        x, y = data
+        return x, medfilt(y, self.spinbox.value())
