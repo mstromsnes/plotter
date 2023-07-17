8dfb4acf9aaf5e5b6a4d1e86d99245cf41afd129
Magne Strømsnes <alaka@Alaka-PC>
2023-07-17 10:12:34 +0200

    Model for temperature

---
+++ raspberry_listener/datamodels/temperature.py
@@ -0,0 +1,59 @@
+from plotstrategies import PlotStrategy
+from collections import defaultdict
+from datamodels import DataSet_Fn, DataTypeModel
+from plotstrategies import LinePlot
+from numpy.typing import NDArray
+from numpy import datetime64, floating
+
+TemperatureData = tuple[NDArray[datetime64], NDArray[floating]]
+
+
+class TemperatureModel(DataTypeModel):
+    SUPPORTED_PLOTS = [LinePlot]
+
+    def __init__(self):
+        super().__init__()
+        self._datalines: dict[str, DataSet_Fn] = {}
+        self._plottype_to_name: dict[type[PlotStrategy], list[str]] = defaultdict(list)
+        self._name_to_plot: dict[str, list[PlotStrategy]] = defaultdict(list)
+        self._name_to_source_name: dict[str, str] = defaultdict(str)
+        self._supported_plots: set[type[PlotStrategy]] = {LinePlot}
+
+    def name(self):
+        return "Temperature"
+
+    def register_data(self, name: str, dataset_fn: DataSet_Fn, source_name: str):
+        if name in self._datalines.keys():
+            raise KeyError(f"{name} already registered")
+        self._datalines[name] = dataset_fn
+        self._name_to_source_name[name] = source_name
+        self._make_plots(name, dataset_fn)
+        self._has_data = True
+
+    def get_source_name(self, name) -> str:
+        return self._name_to_source_name[name]
+
+    def get_data(self, name: str) -> TemperatureData:
+        return self._datalines[name]()
+
+    def get_plots(self, name: str) -> list[PlotStrategy]:
+        return self._name_to_plot[name]
+
+    def get_plot(self, name: str, plot_type: type[PlotStrategy]) -> PlotStrategy:
+        plots = self._name_to_plot[name]
+        for plot in plots:
+            if isinstance(plot, plot_type):
+                return plot
+        raise KeyError
+
+    def get_dataset_names(self) -> set[str]:
+        return set(self._datalines.keys())
+
+    def supported_plots(self) -> set[type[PlotStrategy]]:
+        return self._supported_plots
+
+    def _make_plots(self, name: str, dataset_fn: DataSet_Fn):
+        for plot_type in self.SUPPORTED_PLOTS:
+            plot = plot_type(dataset_fn, model=self, label=name)
+            self._plottype_to_name[plot_type].append(name)
+            self._name_to_plot[name].append(plot)
