cb6bc58e9f71554a27de3a510f1919bab3d653d1
Magne Strømsnes <alaka@Alaka-PC>
2023-07-26 16:42:58 +0200

    Factory pattern for plotwidget
    The subclasses for the plot/draw widgets are all very similar. They
    differ in the navigation toolbar, and in which plotstrategy they
    implement. The code for them mentions the strategy-type they use. At the
    same time, this was not visible at the point the widgets get created,
    and an extra dictionary lookup was used to determine which widget was to
    be made for which strategy. The dictionary was awkwardly placed.
    
    This removes the entire subclass hierarchy. The hierarchy now is
    DrawWidget, which creates the figure, the navigation toolbar, and
    methods for creating and removing axes, and the PlotWidget which holds
    the PlotManager and the DataTypeModel and PlotStrategy. It has methods
    for calling the manager, and the manager will call methods in the
    DrawWidget.
    
    The individual widgets are now built by a factory instead. The previous
    dictionary lookup is turned into a match-block that calls specific
    factory methods. The factory methods create a plotwidget with specific
    configurations. It creates the appropriate manager-type, although only 1
    of these exists at the moment. It sends in the type of plotstrategy the
    manager should be creating. It configures the navigation toolbar
    builder. It then returns a PlotWidget that is configured appropriately.
    The widgets now have different state, but the same type.

--- raspberry_listener/ui/plots/plotwidget.py
+++ raspberry_listener/ui/plots/plotwidget.py
@@ -1,15 +1,83 @@
-from ui.drawwidget import DrawWidget
-from abc import abstractmethod
+from ui.drawwidget import DrawWidget, NavBarBuilder
+from datamodels import DataTypeModel
+from PySide6 import QtWidgets
+from plotstrategies import PlotStrategy
+from plotmanager import PlotManager, OneAxesPlotManager
+import plotstrategies
 
 
 class PlotWidget(DrawWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+    def __init__(
+        self,
+        model: DataTypeModel,
+        manager_type: type[PlotManager],
+        plot_type: type[PlotStrategy],
+        rescale_plot: bool,
+        subplot_kwargs: dict = {},
+        parent: QtWidgets.QWidget | None = None,
+    ):
+        super().__init__(rescale_plot, parent=parent)
+        self.model = model
+        self.manager = manager_type(self, model, subplot_kwargs)
+        self.plot_type = plot_type
 
-    @abstractmethod
     def toggle_data(self, label, checked):
-        pass
+        if checked:
+            self.manager.add_plotting_strategy(label, self.plot_type)
+        else:
+            self.manager.remove_plotting_strategy(label)
 
-    @abstractmethod
     def plot(self):
-        pass
+        self.manager.plot()
+
+
+class PlotWidgetFactory:
+    @classmethod
+    def build(cls, plot_type: type[PlotStrategy], model: DataTypeModel) -> PlotWidget:
+        match plot_type:
+            case plotstrategies.LinePlot:
+                return cls._build_lineplot_widget(model)
+            case plotstrategies.HistogramPlot:
+                return cls._build_histogram_widget(model)
+            case plotstrategies.TimeOfDayPlot:
+                return cls._build_timeofday_widget(model)
+            case _:
+                raise NotImplementedError
+
+    @classmethod
+    def _build_lineplot_widget(cls, model: DataTypeModel) -> PlotWidget:
+        lineplot_widget = PlotWidget(
+            model, OneAxesPlotManager, plotstrategies.LinePlot, rescale_plot=True
+        )
+        lineplot_widget.add_navigation_bar(
+            NavBarBuilder()
+            .navigation_toolbar()
+            .freeze_plot()
+            .simplify_plot()
+            .rescale_plot()
+        )
+        return lineplot_widget
+
+    @classmethod
+    def _build_histogram_widget(cls, model: DataTypeModel) -> PlotWidget:
+        histogram_widget = PlotWidget(
+            model, OneAxesPlotManager, plotstrategies.HistogramPlot, rescale_plot=True
+        )
+        histogram_widget.add_navigation_bar(
+            NavBarBuilder().navigation_toolbar().freeze_plot().rescale_plot()
+        )
+        return histogram_widget
+
+    @classmethod
+    def _build_timeofday_widget(cls, model: DataTypeModel) -> PlotWidget:
+        lineplot_widget = PlotWidget(
+            model,
+            OneAxesPlotManager,
+            plotstrategies.TimeOfDayPlot,
+            rescale_plot=False,
+            subplot_kwargs={"projection": "polar"},
+        )
+        lineplot_widget.add_navigation_bar(
+            NavBarBuilder().navigation_toolbar().freeze_plot()
+        )
+        return lineplot_widget
