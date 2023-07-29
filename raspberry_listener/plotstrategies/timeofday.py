88f7342b6c7974b7510fb50d4b218685a4e90c99
Magne Strømsnes <alaka@Alaka-PC>
2023-07-29 20:44:00 +0200

    Make a flat version of the time-of-day
    This uses normal cartesian coordinates. Styling wise it looks better to
    have the higher values on the bottom.

--- raspberry_listener/plotstrategies/timeofday.py
+++ raspberry_listener/plotstrategies/timeofday.py
@@ -3,10 +3,9 @@ from typing import Sequence
 
 import numpy as np
 import pandas as pd
-from matplotlib.cm import ScalarMappable
+from matplotlib.axes import Axes
 from matplotlib.colors import Colormap, Normalize
 from matplotlib.container import BarContainer
-from matplotlib.figure import Figure
 from matplotlib.projections.polar import PolarAxes
 from matplotlib.ticker import NullFormatter
 from sources import DataNotReadyException
@@ -97,6 +96,132 @@ class TimeOfDayPlot(PlotStrategy):
         frame = pd.DataFrame(data, index=pd.DatetimeIndex(timeseries))
         histogram = self._density_count_by_hour(frame, bin_edges)
         return histogram, bin_edges
+
+    def _density_count_by_hour(self, frame: pd.DataFrame, bins):
+        assert isinstance(frame.index, pd.DatetimeIndex)
+        grouping = frame.groupby(frame.index.hour)
+
+        def count_without_bins(array, bins):
+            count, _ = np.histogram(array, bins, density=True)
+            return count / np.sum(
+                count
+            )  # The density integral is 1, not the sum. They are equal if the bin-width is 1, which is not the case here.
+
+        counts = [count_without_bins(group, bins) for _, group in grouping]
+        counts = np.array(counts)
+        return counts
+
+    def set_tick_formatter(self, ax):
+        ax.yaxis.set_major_formatter(NullFormatter())
+
+    def remove_artist(self):
+        try:
+            [artist.remove() for artist in self.artists]
+            del self.artists
+        except AttributeError:
+            pass
+
+    def set_colorsource(self, colors: Colormap):
+        self.colormap = colors
+
+    def get_colormap(self):
+        return self.colormap
+
+    def get_normalizer(self):
+        try:
+            return self._used_norm
+        except AttributeError:
+            raise PlotNotReadyException
+
+
+class FlatTimeOfDay(PlotStrategy):
+    @staticmethod
+    def name():
+        return "Flat Time of Day"
+
+    def __call__(self, ax: PolarAxes, **kwargs):
+        try:
+            counts, bins, mean_x_values, mean_values = self.get_plot_data()
+        except DataNotReadyException:
+            return
+        try:
+            self.remove_artist()
+        except AttributeError:
+            pass
+        self.plot_stacked_bar(ax, counts, bins, **kwargs)
+        self.set_tick_formatter(ax)
+
+    def get_plot_data(self):
+        full_timeseries, full_data = self.model.get_data(self.label)
+        timeseries = full_timeseries[::1]
+        data = full_data[0::1]
+        counts, bins = self.time_of_day_histogram(timeseries, data)
+        mean_x_values, mean_values = self.time_of_day_mean(timeseries, data)
+        return counts, bins, mean_x_values, mean_values
+    def plot_stacked_bar(
+        self,
+        ax: Axes,
+        counts: np.ndarray,
+        bins: np.ndarray,
+        **kwargs,
+    ):
+        normalizer = Normalize(bins[0], bins[-2])
+        bottom = np.zeros(24)
+        self.artists: Sequence[BarContainer] = []
+        x = range(24)
+        width = 1
+        for i in range(len(bins) - 1):
+            centre_value = (bins[-(i + 1)] + bins[-(i + 1) - 1]) / 2
+            left_value = bins[-(i + 1)]
+            color = self.colormap(X=normalizer(centre_value))
+            height = counts.T[-(i + 1)]
+            artist = ax.bar(
+                x=x,
+                height=height,
+                width=width,
+                bottom=bottom,
+                align="edge",
+                label=f"{left_value:.2f}",
+                color=color,
+                edgecolor=color,
+                **kwargs,
+            )
+            bottom += height
+            self.artists.append(artist)
+        self.set_grid(ax)
+        self._used_norm = normalizer
+
+    def time_of_day_histogram(self, timeseries, data):
+        # Here we get the bins for the entire range of values in the dataset. Each hour can have a different range of values, producing different bins
+        # In order to properly compare we want to use the same bins for every hour
+        first_edge = np.min(data)
+        last_edge = np.max(data)
+        bin_count = min(64, len(np.unique(data)))
+        print(bin_count)
+        bin_edges = np.linspace(
+            first_edge, last_edge, bin_count, endpoint=True, dtype=data.dtype
+        )
+        bin_edges = np.append(bin_edges, bin_edges[-1] + (bin_edges[1] - bin_edges[0]))
+        frame = pd.DataFrame(data, index=pd.DatetimeIndex(timeseries))
+        histogram = self._density_count_by_hour(frame, bin_edges)
+        return histogram, bin_edges
+
+    def time_of_day_mean(self, timeseries, data):
+        frame = pd.DataFrame(data, index=pd.DatetimeIndex(timeseries))
+        assert isinstance(frame.index, pd.DatetimeIndex)
+        frame["timeofday"] = list(
+            # zip(frame.index.hour, frame.index.minute, frame.index.second)
+            zip(frame.index.hour, frame.index.minute)
+        )
+        grouping = frame.groupby("timeofday")
+        mean_data = grouping.mean()
+        x_value_count = mean_data.size
+        x_axis_values = np.linspace(0, 24, x_value_count, endpoint=False)
+        return x_axis_values, mean_data
+
+    def set_grid(self, ax: Axes):
+        ax.set_xlim(0, 24)
+        ax.set_ylim(0, 1)
 
     def _density_count_by_hour(self, frame: pd.DataFrame, bins):
         assert isinstance(frame.index, pd.DatetimeIndex)
