0f1a5e30b4acf5d8034279b3f0d96573cdc35cb7
Magne Strømsnes <alaka@Alaka-PC>
2023-08-09 20:31:59 +0200

    Observer to update views when model updates
    This should perhaps just be done using Qt Signals and Slots, but since
    the model didn't start as a QObject, I've just rolled my own basic
    observer pattern.

--- raspberry_listener/datamodels/basemodels.py
+++ raspberry_listener/datamodels/basemodels.py
@@ -12,22 +12,27 @@ class OneDimensionalTimeSeriesModel(DataTypeModel):
     def __init__(self):
         super().__init__()
         self._datalines: dict[tuple[str, str], DataSet_Fn] = {}
-        self._name_to_source_name: dict[str, str] = defaultdict(str)
+        self._source_name_to_data_name: dict[str, list[str]] = defaultdict(list)
 
+    def _register_data(self, name: str, dataset_fn: DataSet_Fn, source_name: str):
         key = (source_name, name)
         if key in self._datalines.keys():
             raise KeyError(f"{name} from {source_name} already registered")
         self._datalines[key] = dataset_fn
+        self._source_name_to_data_name[source_name].append(name)
         self._has_data = True
 
-    def get_source_name(self, name) -> str:
-        return self._name_to_source_name[name]
+    def get_data_name_from_source(self, source_name: str) -> list[str]:
+        return self._source_name_to_data_name[source_name]
 
     def get_data(self, key: tuple[str, str]) -> OneDimensionalTimeSeries:
         return self._datalines[key]()
 
-    def get_dataset_names(self) -> set[str]:
+    def get_dataset_names(self) -> set[tuple[str, str]]:
         return set(self._datalines.keys())
+
+    def get_source_names(self) -> set[str]:
+        return set(self._source_name_to_data_name.keys())
 
 
 class TemperatureModel(OneDimensionalTimeSeriesModel):
--- raspberry_listener/datamodels/datatypes.py
+++ raspberry_listener/datamodels/datatypes.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, Protocol
+from typing import Any, Callable, Protocol, Self
 
 
 class DataSet_Fn(Protocol):
@@ -15,21 +15,44 @@ class Unit:
     explanation: str
 
 
+Observer = Callable[[str], None]
+
+
 class DataTypeModel(ABC):
     _unit: Unit
 
     def __init__(self):
         self._has_data = False
+        self._observers: set[Observer] = set()
 
     def has_data(self):
         return self._has_data
+
+    def register_observer(self, observer: Observer):
+        self._observers.add(observer)
+
+    def remove_observer(self, observer: Observer):
+        try:
+            self._observers.remove(observer)
+        except KeyError:
+            pass
 
     @abstractmethod
     def name(self) -> str:
         ...
 
-    @abstractmethod
+    # This is essentially a decorator that only decorates the private _register_data method.
     def register_data(self, name: str, dataset_fn: DataSet_Fn, source_name: str):
+        self._register_data(name, dataset_fn, source_name)
+        for obs in self._observers:
+            obs(source_name)
+
+    @abstractmethod
+    def _register_data(self, name: str, dataset_fn: DataSet_Fn, source_name: str):
+        ...
+
+    @abstractmethod
+    def get_data_name_from_source(self, source_name: str) -> list[str]:
         ...
 
     @abstractmethod
@@ -41,7 +64,7 @@ class DataTypeModel(ABC):
         ...
 
     @abstractmethod
-    def get_dataset_names(self) -> set[str]:
+    def get_source_names(self) -> set[str]:
         ...
 
     @classmethod
@@ -51,7 +74,7 @@ class DataTypeModel(ABC):
 
 
 class DataTypeManager:
-    def __init__(self):
+    def __init__(self: Self):
         self._datatypemodels: dict[type[DataTypeModel], DataTypeModel] = {}
 
     def register_datatype(self, datatype: DataTypeModel):
