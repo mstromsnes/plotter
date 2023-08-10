9b696e35c39595d9b941e4041c889979f3a2b718
Magne Strømsnes <alaka@Alaka-PC>
2023-08-10 03:21:33 +0200

    Update test calls

--- raspberry_listener/tests/test_apiclient.py
+++ raspberry_listener/tests/test_apiclient.py
@@ -1,20 +1,20 @@
 from json.decoder import JSONDecodeError
 
 import pytest
-from sources.yr.apiclient import Variant, download_location
-from sources.yr.yr import ARNA, OSLO, Location
+from sources.yr.apiclient import Variant, download_forecast_from_location
+from sources.yr.yrforecast import ARNA, OSLO
 
 
 def test_location():
     # Just test it doesn't raise any exceptions
-    download_location(ARNA, Variant.Compact)
-    download_location(OSLO, Variant.Compact)
+    download_forecast_from_location(ARNA, Variant.Compact)
+    download_forecast_from_location(OSLO, Variant.Compact)
 
-    download_location(ARNA, Variant.Complete)
-    download_location(OSLO, Variant.Complete)
+    download_forecast_from_location(ARNA, Variant.Complete)
+    download_forecast_from_location(OSLO, Variant.Complete)
 
     # Classic returns XML and should give us a JSONDecodeError, but still a 200 OK response code
     with pytest.raises(JSONDecodeError):
-        download_location(ARNA, Variant.Classic)
+        download_forecast_from_location(ARNA, Variant.Classic)
     with pytest.raises(JSONDecodeError):
-        download_location(OSLO, Variant.Classic)
+        download_forecast_from_location(OSLO, Variant.Classic)
