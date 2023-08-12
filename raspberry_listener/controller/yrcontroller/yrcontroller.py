00148eac3cab16a48a79f1aafa86bf06aeea4f0e
Magne Strømsnes <alaka@Alaka-PC>
2023-08-12 19:17:53 +0200

    Load historic locations asynchronously and store in dataframe
    This loads the data into a dataframe, and the controller adapts the
    dataloader into a plottable form for the model, providing numpy arrays.
    
    The controller also injects a median filter, as there was at least some
    bad data in the sets tested.

--- raspberry_listener/controller/yrcontroller/yrcontroller.py
+++ raspberry_listener/controller/yrcontroller/yrcontroller.py
@@ -1,4 +1,9 @@
+from functools import partial
+from typing import Callable
+
+import numpy as np
 from datamodels import HumidityModel, TemperatureModel
+from pandas import Series
 from sources import YrForecast, YrHistoric, get_settings
 
 
@@ -33,7 +38,25 @@ def register_yr_historic_data(
     name: str,
 ):
     def get_data(location: str, variable: str):
-        pass
+        data = yr_historic.data_for_location(location)
+
+        def med_filter(series: Series, window: int) -> Series:
+            return series.rolling(window, min_periods=1).median()
+
+        def extract_data(element_id: str, filter: Callable[[Series], Series]):
+            frame = data[data["elementId"] == element_id]
+            value_series = filter(frame["value"])
+            time = frame["referenceTime"].to_numpy(dtype="datetime64[s]")
+            value = value_series.to_numpy(dtype=np.float32)
+            return time, value
+
+        match variable:
+            case "temperature":
+                return extract_data("air_temperature", partial(med_filter, window=3))
+            case "humidity":
+                return extract_data("relative_humidity", partial(med_filter, window=3))
+            case _:
+                raise KeyError("Unsupported variable type")
 
     settings = get_settings()
     locations: dict[str, str] = settings["frost"]["stations"]
