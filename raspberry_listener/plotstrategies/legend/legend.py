b96d02edd4e4383b4290a5dd200bc2ce0a74d4f3
Magne.Stromsnes <magne.stromsnes@student.uib.no>
2023-08-26 15:36:52 +0200

    Update legends to be more compatible with new axes
    The new multiple axes strategies didn't work well with existing legend
    implementations. Legends now take the item and get handled better.

--- raspberry_listener/plotstrategies/legend/legend.py
+++ raspberry_listener/plotstrategies/legend/legend.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Self, Sequence
+from typing import Self
 
+from datamodels import DataIdentifier
 from matplotlib.axes import Axes
 from matplotlib.cm import ScalarMappable
 from matplotlib.colorbar import Colorbar
@@ -8,61 +9,71 @@ from matplotlib.colors import Normalize
 from matplotlib.figure import Figure
 from matplotlib.legend import Legend
 
-from ..plotstrategy import ColormapPlotStrategy, PlotNotReadyException, PlotStrategy
+from ..plotitem import PlotItem, PlotStatus
+from ..plotstrategy import ColormapPlotStrategy, PlotNotReadyException
 
 
 class LegendStrategy(ABC):
     @abstractmethod
-    def __call__(
-        self,
-        ax: Axes,
-        fig: Figure,
-        strategies: Sequence[PlotStrategy],
-    ):
-        ...
-
-    @abstractmethod
-    def remove_legend(self):
+    def __call__(self, plot_items: list[PlotItem]):
         ...
 
 
 class InternalLegend(LegendStrategy):
     def __init__(self) -> None:
-        self.legend: Legend | None = None
+        self.legends: dict[Axes, Legend] = {}
 
-    def __call__(self, ax: Axes, fig: Figure, strategies):
-        self.remove_legend()
-        self.legend = ax.legend()
+    def __call__(self, plot_items: list[PlotItem]):
+        unique_axes = {
+            plot.ax for plot in plot_items if plot.status != PlotStatus.Disabled
+        }
+        for ax in unique_axes:
+            self.remove_legend(ax)
+        self.legends = {ax: ax.legend() for ax in unique_axes}
 
-    def remove_legend(self):
-        if self.legend is None:
-            return
-        self.legend.remove()
-        self.legend = None
+    def remove_legend(self, ax: Axes):
+        try:
+            self.legends[ax].remove()
+        except KeyError:
+            pass
 
 
 class ExternalLegend(LegendStrategy):
     def __init__(self) -> None:
-        self.legend: Legend | None = None
+        self.legends: dict[Figure, Legend] = {}
 
-    def __call__(self, ax: Axes, fig: Figure, strategies):
-        self.remove_legend()
-        self.legend = fig.legend()
-
-    def remove_legend(self):
-        if self.legend is None:
+    def __call__(self, plot_items: list[PlotItem]):
+        if not plot_items:
             return
-        self.legend.remove()
-        self.legend = None
+        fig = None
+        for plot in plot_items:
+            if plot.visible:
+                fig = plot.figure
+                self.remove_legend(fig)
+                self.legends[fig] = fig.legend()
+
+    def remove_legend(self, fig: Figure):
+        try:
+            self.legends[fig].remove()
+            del self.legends[fig]
+        except KeyError:
+            pass
 
 
 class ColorbarLegend(LegendStrategy):
     def __init__(self: Self):
-        self.colorbar: Colorbar | None = None
+        self.colorbars: dict[DataIdentifier, Colorbar] = {}
 
-    def __call__(self, ax: Axes, fig: Figure, strategies: Sequence[PlotStrategy]):
-        # Only draw the colorbar for the last strategy. We don't really support multiple strategies.
-        strategy = strategies[-1]
+    def __call__(self, plot_items: list[PlotItem]):
+        for plot in plot_items:
+            self.draw_legend(plot)
+            self.set_title(plot)
+
+    def draw_legend(self, plot_item: PlotItem):
+        ax = plot_item.ax
+        strategy = plot_item.plot_strategy
+        dataset = plot_item.identifier
+        fig = ax.figure
         assert isinstance(
             strategy, ColormapPlotStrategy
         ), "Passed an incompatible PlotStrategy type"
@@ -72,12 +83,24 @@ class ColorbarLegend(LegendStrategy):
             mappable = ScalarMappable(norm, cmap)
         except PlotNotReadyException:
             return
-        if self.colorbar is None:
-            self.colorbar = fig.colorbar(mappable)
+        colorbar = self.colorbars.get(dataset)
+        if colorbar is None:
+            self.colorbars[dataset] = fig.colorbar(mappable, ax=ax)
+            self.colorbars[dataset].ax.set_label(f"Colorbar: {str(dataset)}")
         else:
-            self.colorbar.update_normal(mappable)
+            if colorbar.ax in fig.axes:
+                colorbar.update_normal(mappable)
+            else:
+                try:
+                    colorbar.remove()
+                except AttributeError:
+                    pass
+                self.colorbars[dataset] = fig.colorbar(mappable, ax=ax)
+
+    def set_title(self, plot_item: PlotItem):
+        fig = plot_item.ax.figure
+        fig.suptitle(str(plot_item.identifier))
 
     def remove_legend(self):
-        if self.colorbar is None:
-            return
-        self.colorbar.update_normal(ScalarMappable(Normalize(0, 1), "Greys"))
+        for colorbar in self.colorbars.values():
+            colorbar.update_normal(ScalarMappable(Normalize(0, 1), "Greys"))
--- raspberry_listener/plotstrategies/line.py
+++ raspberry_listener/plotstrategies/line.py
@@ -10,32 +10,26 @@ class LinePlot(SingleColorPlotStrategy):
         super().__init__(model, dataset)
         self.color = None
 
-    @staticmethod
-    def name():
-        return "Line"
-
     def __call__(self, ax: Axes, **kwargs):
         x, y = self.model.get_data(self.dataset)
-        try:
-            self.artist.set_xdata(x)
-            self.artist.set_ydata(y)
-            self.artist.recache()
-        except AttributeError:
-            self._artists = ax.plot(
-                x,
-                y,
-                label=f"{self.dataset.data}, {self.dataset.source}",
-                color=self.color,
-                **kwargs,
-            )
+        self.remove_artist()
+        self._artists = ax.plot(
+            x,
+            y,
+            label=f"{self.dataset.data}, {self.dataset.source}",
+            color=self.color,
+            **kwargs,
+        )
 
-    @property
     def artist(self) -> Line2D:
         return self._artists[0]
 
     def remove_artist(self):
-        self.artist.remove()
-        del self._artists
+        try:
+            self.artist().remove()
+            del self._artists
+        except AttributeError:
+            pass
 
     def set_color(self, color: Color):
         self.color = color
