4626c2e9e637ab144652e3ef3295083eb57a42ca
Magne.Stromsnes <magne.stromsnes@student.uib.no>
2023-08-26 15:24:13 +0200

    Introduce a plotitem for managing individual plots
    PlotItem works as a sort of model for plots, moving some responsibility
    away from plotmanager. PlotItem keeps track of where it is being plotted
    by owning its own AxesStrategy and ensuring it plots to the right place.
    This way the manager doesn't have to figure out the axes for each
    strategy itself. It also updates the plot_strategy and axes_strategy
    objects whenever the plot visibility changes, allowing axes to be
    removed or added, or artists removed from graphs without the manager
    needing to worry about it.

---
+++ raspberry_listener/plotstrategies/plotitem/plotitem.py
@@ -0,0 +1,80 @@
+from enum import Enum, auto
+
+from attrs import define, field
+from datamodels import DataIdentifier
+from matplotlib.axes import Axes
+
+from ..axes import AxesStrategy
+from ..plotstrategy import PlotStrategy
+
+
+class PlotStatus(Enum):
+    Enabled = auto()
+    Disabled = auto()
+    Hidden = auto()
+
+
+@define
+class PlotItem:
+    identifier: DataIdentifier
+    plot_strategy: PlotStrategy
+    _status: PlotStatus
+    _axes_strategy: AxesStrategy
+
+    @property
+    def ax(self) -> Axes:
+        print(f"Checking axes for {self.identifier=}")
+        return self._axes_strategy.from_dataidentifier(self.identifier)
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, new_status: PlotStatus):
+        if new_status == self.status:
+            return
+
+        def transition_from_hidden(new_status: PlotStatus):
+            if new_status == PlotStatus.Disabled:
+                self._axes_strategy.remove_plot(self.identifier)
+
+        def transition_from_enabled(new_status: PlotStatus):
+            if new_status == PlotStatus.Disabled:
+                self.plot_strategy.remove_artist()
+                self._axes_strategy.remove_plot(self.identifier)
+            elif new_status == PlotStatus.Hidden:
+                self.plot_strategy.remove_artist()
+
+        def transition_from_disabled(new_status: PlotStatus):
+            if new_status == PlotStatus.Enabled:
+                self._axes_strategy.add_plot(self.identifier)
+            elif new_status == PlotStatus.Hidden:
+                self._axes_strategy.add_plot(self.identifier)
+
+        match self.status:
+            case PlotStatus.Enabled:
+                transition_from_enabled(new_status)
+            case PlotStatus.Disabled:
+                transition_from_disabled(new_status)
+            case PlotStatus.Hidden:
+                transition_from_hidden(new_status)
+        self._status = new_status
+
+    @property
+    def visible(self) -> bool:
+        return self.status == PlotStatus.Enabled
+
+    @property
+    def figure(self):
+        return self.ax.figure
+
+    @property
+    def root_figure(self):
+        fig = self.figure
+        while fig != fig.get_figure():
+            fig = fig.get_figure()
+        return fig
+
+    def draw_plot(self):
+        self.plot_strategy(self.ax)
